58a0e299a3ac3592e553e15489dcd300d20394a5
Mason Karr <karr.mason@gmail.com>
2026-01-05 12:13:33 -0600

    string interpolation practice

---
+++ list.js
@@ -0,0 +1,11 @@
+
+        let name1 =  "Mason";
+        console.log(`My name is ${name1}`)
+  
+ 
+        let coffee = 5;
+        let bagel = 3;
+        let soup = 9;
+        let cost = coffee + (bagel*2) + soup;
+
+    console.log(`Your cost is $${cost}`)
