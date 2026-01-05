35ab2dd99ff33fd443d238f39d1aed29f73044a6
Mason Karr <karr.mason@gmail.com>
2026-01-05 12:25:35 -0600

    added total cost

--- list.js
+++ list.js
@@ -1,11 +1,17 @@
 
-        let name1 =  "Mason";
-        console.log(`My name is ${name1}`)
+let name1 =  "Mason";
+console.log(`My name is ${name1}`)
   
  
-        let coffee = 5;
-        let bagel = 3;
-        let soup = 9;
-        let cost = coffee + (bagel*2) + soup;
+let coffee = 5;
+let bagel = 3;
+let soup = 9;
+let cost = coffee + (bagel*2) + soup;
 
-    console.log(`Your cost is $${cost}`)
+console.log(`Your cost is $${cost}`)
+
+let taxes = .1
+
+let total = cost + (cost * taxes)
+
+console.log(`Actually your total is $${total}, sorry for the confusion.`)
