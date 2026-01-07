dbf7d8b5fec5226da8e11b4b81e2350b60277ce6
Mason Karr <karr.mason@gmail.com>
2026-01-06 19:03:25 -0600

    added user parameter to function.

--- booleans.js
+++ booleans.js
@@ -1,20 +1,22 @@
 
-function timeCounter(hour){
+function timeCounter(hour, user){
 if (hour >= 6 && hour <=12){
-    console.log("Good morning")
+    console.log(`Good morning ${user}`)
 }else if (hour >=13 && hour <=17){
-    console.log("Good afternoon")
-}
-else {
-    console.log("Good night")
+    console.log(`Good afternoon ${user}`)
+}else {
+    console.log(`Good night ${user}`)
 }
 };
 
-
+let hour = 18
+// let user1 = "Mason";
+// let user2 = "Jade";
+// let user3 = "Charlie";
 const night = 1
 const day = 8
 const afternoon = 15
 
-timeCounter(night)
-timeCounter(day)
-timeCounter(afternoon)
+timeCounter(night, "Mason")
+timeCounter(day, "Jade")
+timeCounter(afternoon, "Charlie")
